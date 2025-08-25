31492c5c45dc07330ce72d0e639721f9a33bbc40
Sai Bharath K <saimomdad9@gmail.com>
2025-08-25 12:33:24 +0530

    To check the build

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample{
 	public static void main(String[] args){
 		System.out.println("Sample java");
+		System.out.println("Sample class has also been created..... because of the compilation");
 	}
 }
