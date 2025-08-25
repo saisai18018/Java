0ad9fcf077ddd098a5a9f6d89f816aab2c3f09dd
Sai Bharath K <saimomdad9@gmail.com>
2025-08-25 12:42:29 +0530

    another

--- Sample.java
+++ Sample.java
@@ -2,5 +2,8 @@ public class Sample{
 	public static void main(String[] args){
 		System.out.println("Sample java");
 		System.out.println("Sample class has also been created..... because of the compilation");
+				System.out.println("Sample class has also been created..... because of the compilation");
+				System.out.println("Sample class has also been created..... because of the compilation");
+
 	}
 }
